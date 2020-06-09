52b436c767a4f5a8ab9d474c94e7a6629e13e598
Loriano Storchi <loriano@storchi.org>
2020-06-09 16:32:18 +0200

    add Hyds and 3d writer

--- preparedata.py
+++ preparedata.py
@@ -1,6 +1,7 @@
 import pandas as pd 
 import argparse
 from rdkit import Chem
+from rdkit.Chem import AllChem
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -13,8 +14,21 @@ if __name__ == "__main__":
                     
     data = pd.read_excel(filename)
 
-    for ss in data["SMILES"]:
+    dim = len(data["SMILES"])
+    for idx, ss in enumerate(data["SMILES"]):
         mol = Chem.MolFromSmiles(str(ss))
+
+        AllChem.Compute2DCoords(mol)
+        AllChem.EmbedMolecule(mol, randomSeed=0xf00d) 
+        Chem.Kekulize(mol)
+        mol_3D = Chem.AddHs(mol)
+        AllChem.EmbedMolecule(mol_3D, randomSeed=0xf00d)
+
+        fout = Chem.SDWriter("molecule_"+str(idx+1)+".mol")
+        fout.write(mol_3D)
+        fout.close()
+
         logd = data[data["SMILES"] == ss]["LogD"].values[0]
-        print("Mol has %5d "%(mol.GetNumAtoms()), " atoms and LogD %10.5f"%(logd))
+        print("Mol %10d of %10d has %5d "%(idx+1, dim, mol.GetNumAtoms()), 
+                " atoms and LogD %10.5f"%(logd))
 
